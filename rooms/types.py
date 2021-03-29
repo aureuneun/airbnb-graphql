048dd59acddd3f124f05f53ea4c182ca72ca0d82
aureuneun <aureuneun@gmail.com>
2021-03-29 23:14:23 +0900

    feat: dynamic fields

--- rooms/types.py
+++ rooms/types.py
@@ -4,8 +4,17 @@ from rooms.models import Room
 
 
 class RoomType(DjangoObjectType):
+
+    is_fav = graphene.Boolean()
+
     class Meta:
         model = Room
+
+    def resolve_is_fav(parent, info):
+        user = info.context.user
+        if user.is_authenticated:
+            return parent in user.favs.all()
+        return False
 
 
 class RoomList(graphene.ObjectType):
